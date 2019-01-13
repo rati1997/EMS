f3a8e49e2f5fa62926671e591a6b6c304f8bd69b
rati <ranjanrati007@gmail.com>
2019-01-14 00:18:56 +0530

    by Rati

---
+++ EMS.java
@@ -0,0 +1,248 @@
+import java.sql.*;  
+import java.util.Scanner;
+class EMS{ 
+    
+    Scanner in = new Scanner(System.in);
+    
+    
+    
+    public void create(int x){
+        
+        try{
+        Class.forName("com.mysql.jdbc.Driver");  
+        Connection con=DriverManager.getConnection(  
+        "jdbc:mysql://localhost:3306/EMS1","rati","root");  
+        //here sonoo is database name, root is username and password  
+        Statement stmt=con.createStatement(); 
+    
+        System.out.println("Enter the following details: \nEmployee Id: ");
+        int Emp_id=in.nextInt();
+        in.nextLine();
+            
+        if(x==1){
+            String sq= "delete from EMPLOYEE where Emp_id="+Emp_id+";";
+            int r = stmt.executeUpdate(sq);
+        }
+        
+        System.out.println("Name: ");
+        String Name = in.nextLine();
+        
+        System.out.println("Designation: ");
+        String Designation = in.nextLine();
+        
+        System.out.println("Salary: ");
+        String Salary = in.nextLine();
+        
+        System.out.println("Department:\n1.Sales\n2.Marketing\n3.Tech\n4.Analyst\n");
+        int Dept_id = in.nextInt();
+        /*Switch(dept){
+            case '1':
+                        
+                        break;
+            case '2':
+                        break;
+            case '3':
+                        break;
+            case '4':
+                        break;
+            default:
+                        System.out.println("Invalid choice");
+            
+            
+        }*/
+        System.out.println("Number of Addresses: ");
+        int numAdd = in.nextInt();
+        in.nextLine();
+        String[] Address = new String[numAdd];
+        for(int i =0;i<numAdd;i++){
+            System.out.println("Address "+i+" : ");
+            Address[i] = in.nextLine();
+        }
+        
+        System.out.println("Number of contacts: ");
+        int numCon = in.nextInt();
+        in.nextLine();
+        String[] Contact = new String[numCon];
+        for(int i =0;i<numCon;i++){
+            System.out.println("Contact "+i+" : ");
+            Contact[i] = in.nextLine();
+        }
+        
+        System.out.println("Number of vehicle: ");
+        int numVeh = in.nextInt();
+        in.nextLine();
+        String [][] Vehicle = new String[numVeh][2];
+        
+        for(int i =0;i<numVeh;i++){
+            System.out.println("Vehicle "+i+" details: ");
+            System.out.println("Registration Number: ");
+            Vehicle[i][0] = in.nextLine();
+            System.out.println("Vehicle type: ");
+            Vehicle[i][1] = in.nextLine();
+        }
+        
+        
+        String sql1 = "insert into EMPLOYEE(Emp_id,Name, Designation,Salary,Dept_id)values('"+Emp_id+"','"+Name+"','"+Designation+"','"+Salary+"','"+Dept_id+"');";
+            stmt.executeUpdate(sql1);
+        for(int i=0;i<numAdd;i++){
+            String sql2 = "insert into ADDRESS(Line1,Emp_Id)values('"+Address[i]+"','"+Emp_id+"');";
+            stmt.executeUpdate(sql2);
+        }
+        for(int i=0;i<numCon;i++){
+            String sql3 = "insert into CONTACT(Phone_no,Emp_Id)values('"+Contact[i]+"','"+Emp_id+"');";
+            stmt.executeUpdate(sql3);
+        }
+        
+        for(int i=0;i<numVeh;i++){
+            String sql4 = "insert into VEHICLE(Reg_no,Type,Emp_Id)values('"+Vehicle[i][0]+"','"+Vehicle[i][1]  +"','"+Emp_id+"');";
+            stmt.executeUpdate(sql4);
+        }
+            
+
+        con.close();
+        }catch(Exception e){ System.out.println(e);}
+        
+    }
+    
+    
+    
+    
+    public void retrieve(){
+        try{
+        System.out.println("Enter the Emp_id to fetch data: ");
+        int Emp_id = in.nextInt();
+        String sqlE = "select Name,Designation,Salary,Dept_name from EMPLOYEE natural join DEPARTMENT where Emp_id ="+Emp_id+";";
+        
+        Class.forName("com.mysql.jdbc.Driver");  
+        Connection con=DriverManager.getConnection(  
+        "jdbc:mysql://localhost:3306/EMS1","rati","root");  
+        //here sonoo is database name, root is username and password  
+        Statement stmt=con.createStatement();    
+        ResultSet rs = stmt.executeQuery(sqlE);
+            
+        String sqlA = "select Line1 from ADDRESS where Emp_id ="+Emp_id+";"; 
+        Statement stmt1=con.createStatement(); 
+        ResultSet rs1 = stmt1.executeQuery(sqlA);
+            
+        String sqlC = "select Phone_no from CONTACT where Emp_id="+Emp_id+";";   
+        Statement stmt2=con.createStatement(); 
+        ResultSet rs2 = stmt2.executeQuery(sqlC);
+            
+        String sqlV = "select Reg_no,Type from VEHICLE where Emp_id="+Emp_id+";";
+        Statement stmt3=con.createStatement(); 
+        ResultSet rs3 = stmt3.executeQuery(sqlV);
+           
+        System.out.println("______________________________________________________________________________________________________\nNAME  | DESIGNATION  | SALARY | DEPARTMENT | ADDRESS  |   PHONE NO    | VEHICLE_REG-NO  |VEHICLE_TYPE\n------------------------------------------------------------------------------------------------------");
+        while(rs.next())
+            System.out.print(rs.getString(1)+" |     "+rs.getString(2)+"     |  "+rs.getString(3)+"  | "+rs.getString(4));
+        while(rs1.next())
+            System.out.print("   |   "+rs1.getString(1));
+        while(rs2.next())
+            System.out.print("  |  "+rs2.getString(1));
+        while(rs3.next())
+            System.out.println("  |    "+rs3.getString(1)+"      |  "+rs3.getString(2)+" |");
+            
+        System.out.println("______________________________________________________________________________________________________");
+        con.close();
+        }catch(Exception e){System.out.println(e);}
+        
+        
+    }
+    
+    public void delete(){
+        
+        try{
+            
+            System.out.println("Enter the Employee id: ");
+            int Emp_id = in.nextInt();
+            in.nextLine();
+            System.out.println("Enter the field to be deleted\n1.Employee Info\n2.Vehicle Info\n3.Contact Info\n4.Employee's Address\n5.Department");
+            int inp = in.nextInt();
+            in.nextLine();
+            String sql="";
+            switch(inp){
+            case 1: 
+                        sql = "delete from EMPLOYEE where Emp_id ="+Emp_id+";";
+                        break;
+            case 2:
+                        sql = "delete from VEHICLE where Emp_id ="+Emp_id+";";
+                        break;
+            case 3:
+                        sql = "delete from CONTACT where Emp_id ="+Emp_id+";";
+                        break;
+            case 4:
+                        sql = "delete from ADDRESS where Emp_id ="+Emp_id+";";
+                        break;
+            case 5:
+                        sql = "delete from DEPARTMENT where Emp_id ="+Emp_id+";";
+                        break;
+            default:
+                        System.out.println("Invalid choice"); 
+            
+        } 
+            Class.forName("com.mysql.jdbc.Driver");  
+            Connection con=DriverManager.getConnection(  
+            "jdbc:mysql://localhost:3306/EMS1","rati","root");  
+            //here sonoo is database name, root is username and password  
+            Statement stmt=con.createStatement(); 
+            int rows = stmt.executeUpdate(sql);
+         
+        }catch(Exception e){System.out.println(e);}
+           
+        
+    }
+    
+    
+    
+    
+    public static void main(String args[]){  
+    try{  
+        
+            Test ob = new Test();
+        
+    /*
+            Class.forName("com.mysql.jdbc.Driver");  
+            Connection con=DriverManager.getConnection(  
+            "jdbc:mysql://localhost:3306/EMS","rati","root");  
+            //here sonoo is database name, root is username and password  
+            Statement stmt=con.createStatement(); 
+    */
+            
+        
+        
+            System.out.println("Enter\n1.Create\n2.Retrieve\n3.Update\n4.Delete an entry ");
+            int i = ob.in.nextInt();
+        
+            switch(i)
+            {
+                case 1:     ob.create(0);
+                            break;
+                case 2:
+                            ob.retrieve();
+                            break;
+                case 3:
+                            ob.create(1);
+                            break;
+                case 4: 
+                            ob.delete();
+                            break;
+                default:
+                            System.out.println("Invalid choice  Try again");
+                            break;
+                
+                   
+            }
+        
+  /*          String var = "Name";
+            ResultSet rs=stmt.executeQuery("select "+var+" from EMPLOYEE limit 5");  
+            while(rs.next()) {
+                System.out.println(rs.getString(1));
+                //System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3)+" "+rs.getString(4)); 
+            }
+            
+
+            con.close();  
+*/
+        }catch(Exception e){ System.out.println(e);}  
+    }  
+}  
---
+++ EMS1.sql
@@ -0,0 +1,156 @@
+-- MySQL dump 10.13  Distrib 5.7.24, for Linux (x86_64)
+--
+-- Host: localhost    Database: EMS1
+-- ------------------------------------------------------
+-- Server version	5.7.24-0ubuntu0.18.10.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `ADDRESS`
+--
+
+DROP TABLE IF EXISTS `ADDRESS`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ADDRESS` (
+  `Line1` varchar(50) DEFAULT NULL,
+  `Emp_id` int(11) NOT NULL,
+  `Add_id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`Add_id`),
+  KEY `fk_add` (`Emp_id`),
+  CONSTRAINT `ADDRESS_ibfk_1` FOREIGN KEY (`Emp_id`) REFERENCES `EMPLOYEE` (`Emp_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ADDRESS`
+--
+
+LOCK TABLES `ADDRESS` WRITE;
+/*!40000 ALTER TABLE `ADDRESS` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ADDRESS` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `CONTACT`
+--
+
+DROP TABLE IF EXISTS `CONTACT`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `CONTACT` (
+  `Phone_no` varchar(10) NOT NULL,
+  `Emp_id` int(11) NOT NULL,
+  PRIMARY KEY (`Phone_no`),
+  KEY `fk_con` (`Emp_id`),
+  CONSTRAINT `CONTACT_ibfk_1` FOREIGN KEY (`Emp_id`) REFERENCES `EMPLOYEE` (`Emp_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `CONTACT`
+--
+
+LOCK TABLES `CONTACT` WRITE;
+/*!40000 ALTER TABLE `CONTACT` DISABLE KEYS */;
+/*!40000 ALTER TABLE `CONTACT` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `DEPARTMENT`
+--
+
+DROP TABLE IF EXISTS `DEPARTMENT`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `DEPARTMENT` (
+  `Dept_id` int(11) NOT NULL,
+  `Dept_name` varchar(25) NOT NULL,
+  PRIMARY KEY (`Dept_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `DEPARTMENT`
+--
+
+LOCK TABLES `DEPARTMENT` WRITE;
+/*!40000 ALTER TABLE `DEPARTMENT` DISABLE KEYS */;
+INSERT INTO `DEPARTMENT` VALUES (1,'Sales'),(2,'Marketing'),(3,'Tech'),(4,'Analyst');
+/*!40000 ALTER TABLE `DEPARTMENT` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `EMPLOYEE`
+--
+
+DROP TABLE IF EXISTS `EMPLOYEE`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `EMPLOYEE` (
+  `Emp_id` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(25) NOT NULL,
+  `Designation` varchar(25) DEFAULT NULL,
+  `Salary` int(11) NOT NULL,
+  `Dept_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Emp_id`),
+  KEY `fk_dept` (`Dept_id`),
+  CONSTRAINT `EMPLOYEE_ibfk_1` FOREIGN KEY (`Dept_id`) REFERENCES `DEPARTMENT` (`Dept_id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `EMPLOYEE`
+--
+
+LOCK TABLES `EMPLOYEE` WRITE;
+/*!40000 ALTER TABLE `EMPLOYEE` DISABLE KEYS */;
+/*!40000 ALTER TABLE `EMPLOYEE` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `VEHICLE`
+--
+
+DROP TABLE IF EXISTS `VEHICLE`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `VEHICLE` (
+  `Reg_no` varchar(10) NOT NULL,
+  `Type` varchar(10) NOT NULL,
+  `Emp_id` int(11) NOT NULL,
+  PRIMARY KEY (`Reg_no`),
+  KEY `fk_veh` (`Emp_id`),
+  CONSTRAINT `VEHICLE_ibfk_1` FOREIGN KEY (`Emp_id`) REFERENCES `EMPLOYEE` (`Emp_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `VEHICLE`
+--
+
+LOCK TABLES `VEHICLE` WRITE;
+/*!40000 ALTER TABLE `VEHICLE` DISABLE KEYS */;
+/*!40000 ALTER TABLE `VEHICLE` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-01-14  0:10:12
